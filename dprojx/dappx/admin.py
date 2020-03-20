20b74610c8cf50fc4e083ee2f739b1e522842f2a
ferd <ferdinand.vettermann@unirostock.de>
2020-03-20 09:32:19 -0700

    first commit

---
+++ dprojx/dappx/admin.py
@@ -0,0 +1,5 @@
+# dappx/admin.py
+from django.contrib import admin
+from dappx.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ dprojx/dprojx/urls.py
@@ -0,0 +1,12 @@
+# dprojx/urls.py
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url,include
+from dappx import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$',views.index,name='index'),
+    url(r'^special/',views.special,name='special'),
+    url(r'^dappx/',include('dappx.urls')),
+    url(r'^logout/$', views.user_logout, name='logout'),
+]
